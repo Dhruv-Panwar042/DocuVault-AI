237119fccbd85a29998cd614bb79b6d77c93a122
Dhruv-Panwar042 <dhruvspanwar12@gmail.com>
2026-08-30 23:12:54 +0530

    Changed the gemini model

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ embeddings = HuggingFaceEmbeddings(
 )
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-2.0-flash",
     google_api_key=GOOGLE_API_KEY,
     temperature=0.3
 )
